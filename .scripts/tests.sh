199876a8ca62924914bc2c0bcfe6a7ab87fdba0d
Inguss Neilands <Inguss.Neilands@gmail.com>
2020-06-20 16:04:57 +0100

    testing

---
+++ .scripts/tests.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+DATE=$(date +%Y.%m.%d.)
+NAME=`jq .name info.json -r`
+VERSION=`jq .version info.json -r`
+
+echo ""
+echo "Searching Release Version in changelog.txt"
+echo ""
+# if ! grep "$VERSION[[:cntrl:]]*$" changelog.txt > /dev/null; then
+if ! grep "$VERSION[[:cntrl:]]*$" changelog.txt; then
+  echo "ERROR: Current version ${VERSION} not found in 'changelog.txt'. Please add ingame changelog for current version." 1>&2
+  echo "See https://forums.factorio.com/viewtopic.php?f=25&t=67140 for mor information." 1>&2
+  echo ""
+  exit 1
+fi
+
+echo ""
+echo "Searching Release Date in changelog.txt"
+echo ""
+# if ! grep "$DATE" changelog.txt > /dev/null; then
+if ! grep "$DATE" changelog.txt; then
+  echo ""
+  echo "ERROR: Current date ${DATE} not found in 'changelog.txt'. Please add ingame changelog with current date." 1>&2
+  echo "See https://forums.factorio.com/viewtopic.php?f=25&t=67140 for mor information." 1>&2
+  echo ""
+  exit 1
+fi
+
+echo ""
+echo "Searching Release Version in mods.factorio.com"
+echo ""
+
+curl -s https://mods.factorio.com/api/mods/${NAME}/full | jq -e ".releases[] | select(.version == \"${VERSION}\")"
+STATUS_CODE=$?
+
+if [[ $STATUS_CODE -ne 4 ]]; then
+  echo ""
+  echo "Release already exists, skipping"
+  echo ""
+  exit 1
+else
+  echo "Release ${VERSION} not found"
+fi
+
+echo ""
--- makefile
+++ makefile
@@ -1,30 +1,17 @@
 # Makefile
 
-NAME = $(shell jq .name info.json -r)
-VERSION = $(shell jq .version info.json -r)
+# NAME = $(shell jq .name info.json -r)
+# VERSION = $(shell jq .version info.json -r)
 
 help:
 	@echo "Available Commands"
-	@echo "\tmake build"
-	@echo "\tmake release"
+	@echo "\tmake tests"
+	@echo "\tmake zip"
 
 .DEFAULT_GOAL := help
 
-build:
-	mkdir -p $(NAME)_$(VERSION)
-	mkdir -p $(NAME)_$(VERSION)/$(NAME)
+tests:
+	@bash ./.scripts/tests.sh
 
-	cp -r locale $(NAME)_$(VERSION)/$(NAME)
-	cp -r resources $(NAME)_$(VERSION)/$(NAME)
-	cp -r changelog.txt $(NAME)_$(VERSION)/$(NAME)
-	cp -r info.json $(NAME)_$(VERSION)/$(NAME)
-	cp -r LICENSE $(NAME)_$(VERSION)/$(NAME)
-	cp -r *.lua $(NAME)_$(VERSION)/$(NAME)
-
-	rm -rf $(NAME)_$(VERSION).zip || true
-	cd $(NAME)_$(VERSION) && zip -r ../$(NAME)_$(VERSION).zip $(NAME)
-
-	rm -rf $(NAME)_$(VERSION)
-
-release:
-	@bash ./.scripts/release.sh
+zip:
+	@bash ./.scripts/zip.sh
